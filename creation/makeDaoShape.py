1785e6bbd47fe7c58d9f74e2f6a8186b73dc76b1
headfire <headfire@yandex.ru>
2020-09-13 18:31:58 +0300

    New viewer style - zviewer

--- creation/makeDaoShape.py
+++ creation/makeDaoShape.py
@@ -520,7 +520,7 @@ if __name__ == '__main__':
     #do('screen', 'slide_02_DaoConcept')
     #do('screen', 'slide_03_DaoSecPrincipe')
     #do('screen', 'slide_04_DaoManySec')
-    #do('screen', 'slide_05_DaoSkinning')
+    do('screen', 'slide_05_DaoSkinning')
     #do('screen', 'slide_06_DaoComplete')
     #do('screen', 'slide_07_DaoWithCase')
     
@@ -528,7 +528,7 @@ if __name__ == '__main__':
     #do('web', 'slide_02_DaoConcept')
     #do('web', 'slide_03_DaoSecPrincipe')
     #do('web', 'slide_04_DaoManySec')
-    do('web', 'slide_05_DaoSkinning')
+    #do('web', 'slide_05_DaoSkinning')
     #do('web', 'slide_06_DaoComplete')
     #do('web', 'slide_07_DaoWithCase')
     
